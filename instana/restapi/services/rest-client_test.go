7a94d14b1ca076f2cef7f4becd2c1fb9a8ed4386
Florian Gessner <florian.gessner@tis.biz>
2019-03-19 21:48:16 +0200

    cleanup test implementation

--- instana/restapi/services/rest-client_test.go
+++ instana/restapi/services/rest-client_test.go
@@ -16,132 +16,66 @@ const testID = "test-1234"
 const testData = "testData"
 
 func TestShouldReturnDataForSuccessfulGetOneRequest(t *testing.T) {
-	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodGet, "/api"+testPath+"/"+testID, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(testData))
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServerWithOKResponseCode(http.MethodGet, "/api"+testPath+"/"+testID)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
 	response, err := restClient.GetOne(testID, testPath)
 
-	if err != nil {
-		t.Fatalf("Expected no error to be returned but got %s", err)
-	}
-	responseString := string(response)
-	if responseString != testData {
-		t.Fatalf("Expected test data to be returned but got %s", responseString)
-	}
+	verifySuccessfullGetOrPut(response, err, t)
 }
 
 func TestShouldReturnErrorMessageForGetOneRequestWhenStatusIsNotASuccessStatusAndNotEnityNotFound(t *testing.T) {
 	statusCode := http.StatusBadRequest
-	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodGet, "/api"+testPath+"/"+testID, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(statusCode)
-		w.Write([]byte(testData))
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServer(http.MethodGet, "/api"+testPath+"/"+testID, statusCode)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
 	_, err := restClient.GetOne(testID, testPath)
 
-	if err == nil || !strings.Contains(err.Error(), strconv.Itoa(statusCode)) {
-		t.Fatalf("Expected to receive error message with status Code %d", statusCode)
-	}
+	verifyFailedCallWithStatusCodeIsResponse(err, statusCode, t)
 }
 
 func TestShouldReturnDataForSuccessfulGetAllRequest(t *testing.T) {
-	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodGet, "/api"+testPath, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(testData))
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServerWithOKResponseCode(http.MethodGet, "/api"+testPath)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
 	response, err := restClient.GetAll(testPath)
 
-	if err != nil {
-		t.Fatalf("Expected no error to be returned but got %s", err)
-	}
-	responseString := string(response)
-	if responseString != testData {
-		t.Fatalf("Expected test data to be returned but got %s", responseString)
-	}
+	verifySuccessfullGetOrPut(response, err, t)
 }
 
 func TestShouldReturnErrorMessageForGetAllRequestWhenStatusIsNotASuccessStatusAndNotEnityNotFound(t *testing.T) {
 	statusCode := http.StatusBadRequest
-	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodGet, "/api"+testPath, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(statusCode)
-		w.Write([]byte(testData))
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServer(http.MethodGet, "/api"+testPath, statusCode)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
 	_, err := restClient.GetAll(testPath)
 
-	if err == nil || !strings.Contains(err.Error(), strconv.Itoa(statusCode)) {
-		t.Fatalf("Expected to receive error message with status Code %d", statusCode)
-	}
+	verifyFailedCallWithStatusCodeIsResponse(err, statusCode, t)
 }
 
 func TestShouldReturnDataForSuccessfulPutRequest(t *testing.T) {
-	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodPut, "/api"+testPath+"/"+testID, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(testData))
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServerWithOKResponseCode(http.MethodPut, "/api"+testPath+"/"+testID)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
 	response, err := restClient.Put(testDataObject{id: testID}, testPath)
 
-	if err != nil {
-		t.Fatalf("Expected no error to be returned but got %s", err)
-	}
-	responseString := string(response)
-	if responseString != testData {
-		t.Fatalf("Expected test data to be returned but got %s", responseString)
-	}
+	verifySuccessfullGetOrPut(response, err, t)
 }
 
 func TestShouldReturnErrorMessageForPutRequestWhenStatusIsNotASuccessStatusAndNotEnityNotFound(t *testing.T) {
 	statusCode := http.StatusBadRequest
-	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodPut, "/api"+testPath+"/"+testID, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(statusCode)
-		w.Write([]byte(testData))
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServer(http.MethodPut, "/api"+testPath+"/"+testID, statusCode)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
 	_, err := restClient.Put(testDataObject{id: testID}, testPath)
 
-	if err == nil || !strings.Contains(err.Error(), strconv.Itoa(statusCode)) {
-		t.Fatalf("Expected to receive error message with status Code %d", statusCode)
-	}
+	verifyFailedCallWithStatusCodeIsResponse(err, statusCode, t)
 }
 
 type testDataObject struct {
@@ -160,12 +94,7 @@ func (tdo testDataObject) Validate() error {
 
 func TestShouldReturnNothingForSuccessfulDeleteRequest(t *testing.T) {
 	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodDelete, "/api"+testPath+"/"+testID, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(http.StatusOK)
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServerWithOKResponseCode(http.MethodDelete, "/api"+testPath+"/"+testID)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
@@ -178,18 +107,42 @@ func TestShouldReturnNothingForSuccessfulDeleteRequest(t *testing.T) {
 
 func TestShouldReturnErrorMessageForDeleteRequestWhenStatusIsNotASuccessStatusAndNotEnityNotFound(t *testing.T) {
 	statusCode := http.StatusBadRequest
-	testutils.DeactivateTLSServerCertificateVerification()
-	httpServer := testutils.NewTestHTTPServer()
-	httpServer.AddRoute(http.MethodDelete, "/api"+testPath+"/"+testID, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
-		w.WriteHeader(statusCode)
-	})
-	httpServer.Start()
+	httpServer := setupAndStartHttpServer(http.MethodDelete, "/api"+testPath+"/"+testID, statusCode)
 	defer httpServer.Close()
 
 	restClient := NewClient("api-token", fmt.Sprintf("localhost:%d", httpServer.GetPort()))
 	err := restClient.Delete(testID, testPath)
 
+	verifyFailedCallWithStatusCodeIsResponse(err, statusCode, t)
+}
+
+func setupAndStartHttpServerWithOKResponseCode(httpMethod string, fullPath string) *testutils.TestHTTPServer {
+	return setupAndStartHttpServer(httpMethod, fullPath, 200)
+}
+
+func setupAndStartHttpServer(httpMethod string, fullPath string, statusCode int) *testutils.TestHTTPServer {
+	testutils.DeactivateTLSServerCertificateVerification()
+	httpServer := testutils.NewTestHTTPServer()
+	httpServer.AddRoute(httpMethod, fullPath, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+		w.WriteHeader(statusCode)
+		w.Write([]byte(testData))
+	})
+	httpServer.Start()
+	return httpServer
+}
+
+func verifySuccessfullGetOrPut(response []byte, err error, t *testing.T) {
+	if err != nil {
+		t.Fatalf("Expected no error to be returned but got %s", err)
+	}
+	responseString := string(response)
+	if responseString != testData {
+		t.Fatalf("Expected test data to be returned but got %s", responseString)
+	}
+}
+
+func verifyFailedCallWithStatusCodeIsResponse(err error, statusCode int, t *testing.T) {
 	if err == nil || !strings.Contains(err.Error(), strconv.Itoa(statusCode)) {
 		t.Fatalf("Expected to receive error message with status Code %d", statusCode)
 	}
