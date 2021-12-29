8907880f26dea282a41bb1060da325a1f3c1e859
Florian Gessner <flo.gessner@gmail.com>
2021-12-29 21:54:24 +0100

    #74: fix lint issues

--- instana/restapi/application-alert-config.go
+++ instana/restapi/application-alert-config.go
@@ -19,13 +19,13 @@ type ApplicationAlertRule struct {
 
 //IncludedEndpoint custom type to include of a specific endpoint in an alert config
 type IncludedEndpoint struct {
-	EndpointId string `json:"endpointId"`
+	EndpointID string `json:"endpointId"`
 	Inclusive  bool   `json:"inclusive"`
 }
 
 //IncludedService custom type to include of a specific service in an alert config
 type IncludedService struct {
-	ServiceId string `json:"serviceId"`
+	ServiceID string `json:"serviceId"`
 	Inclusive bool   `json:"inclusive"`
 
 	Endpoints map[string]IncludedEndpoint `json:"endpoints"`
@@ -33,7 +33,7 @@ type IncludedService struct {
 
 //IncludedApplication custom type to include specific applications in an alert config
 type IncludedApplication struct {
-	ApplicationId string `json:"applicationId"`
+	ApplicationID string `json:"applicationId"`
 	Inclusive     bool   `json:"inclusive"`
 
 	Services map[string]IncludedService `json:"services"`
