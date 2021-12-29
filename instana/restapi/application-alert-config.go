e8aa3ec636ce931e9ff29748cb030b9b9d426709
Florian Gessner <flo.gessner@gmail.com>
2021-12-29 21:03:23 +0100

    #74: add tests

--- instana/restapi/application-alert-config.go
+++ instana/restapi/application-alert-config.go
@@ -17,41 +17,6 @@ type ApplicationAlertRule struct {
 	Operator *ExpressionOperator `json:"operator"`
 }
 
-//ApplicationAlertEvaluationType custom type representing the application alert evaluation type from the instana API
-type ApplicationAlertEvaluationType string
-
-//ApplicationAlertEvaluationTypes custom type representing a slice of ApplicationAlertEvaluationType
-type ApplicationAlertEvaluationTypes []ApplicationAlertEvaluationType
-
-//IsSupported check if the provided evaluation type is supported
-func (types ApplicationAlertEvaluationTypes) IsSupported(evalType ApplicationAlertEvaluationType) bool {
-	for _, t := range types {
-		if t == evalType {
-			return true
-		}
-	}
-	return false
-}
-
-//ToStringSlice Returns the corresponding string representations
-func (types ApplicationAlertEvaluationTypes) ToStringSlice() []string {
-	result := make([]string, len(types))
-	for i, v := range types {
-		result[i] = string(v)
-	}
-	return result
-}
-
-const (
-	//EvaluationTypePerApplication constant value for ApplicationAlertEvaluationType PER_AP
-	EvaluationTypePerApplication = ApplicationAlertEvaluationType("PER_AP")
-	//EvaluationTypePerApplicationService constant value for ApplicationAlertEvaluationType PER_AP_SERVICE
-	EvaluationTypePerApplicationService = ApplicationAlertEvaluationType("PER_AP_SERVICE")
-)
-
-//SupportedApplicationAlertEvaluationTypes list of all supported ApplicationAlertEvaluationTypes
-var SupportedApplicationAlertEvaluationTypes = ApplicationAlertEvaluationTypes{EvaluationTypePerApplication, EvaluationTypePerApplicationService}
-
 //IncludedEndpoint custom type to include of a specific endpoint in an alert config
 type IncludedEndpoint struct {
 	Name      string `json:"name"`
---
+++ instana/restapi/application-alert-evaluation-type_test.go
@@ -0,0 +1,25 @@
+package restapi_test
+
+import (
+	"testing"
+
+	. "github.com/gessnerfl/terraform-provider-instana/instana/restapi"
+	"github.com/stretchr/testify/require"
+)
+
+func TestShouldReturnTrueForAllSupportedApplicationAlertEvaluationTypes(t *testing.T) {
+	for _, v := range SupportedApplicationAlertEvaluationTypes {
+		require.True(t, SupportedApplicationAlertEvaluationTypes.IsSupported(v))
+	}
+}
+
+func TestShouldReturnFalseForAllNonSupportedApplicationAlertEvaluationTypes(t *testing.T) {
+	for _, v := range []string{"FOO", "BAR", "INVALID"} {
+		require.False(t, SupportedApplicationAlertEvaluationTypes.IsSupported(ApplicationAlertEvaluationType(v)))
+	}
+}
+
+func TestShouldReturnSupportedApplicationAlertEvaluationTypesAsStringSlice(t *testing.T) {
+	expected := []string{"PER_AP", "PER_AP_SERVICE"}
+	require.Equal(t, expected, SupportedApplicationAlertEvaluationTypes.ToStringSlice())
+}
