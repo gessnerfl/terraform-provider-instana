a3f3199d992a1bee7089e4d542ec94b69a727aad
Florian Gessner <florian.gessner@tis.biz>
2022-03-31 14:32:08 +0200

    #74: add test for application-alert-config-unmarshaller.go

---
+++ instana/restapi/application-alert-config-unmarshaller_test.go
@@ -0,0 +1,103 @@
+package restapi_test
+
+import (
+	"encoding/json"
+	. "github.com/gessnerfl/terraform-provider-instana/instana/restapi"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestShouldSuccessfullyUnmarshalApplicationAlertConfig(t *testing.T) {
+	thresholdValue := 123.3
+	thresholdLastUpdate := int64(12345)
+	applicationAlertConfig := ApplicationAlertConfig{
+		ID:              "test-application-alert-config-id",
+		AlertChannelIDs: []string{"channel-2", "channel-1"},
+		Applications: map[string]IncludedApplication{
+			"app-1": {
+				ApplicationID: "app-1",
+				Inclusive:     true,
+				Services: map[string]IncludedService{
+					"srv-1": {
+						ServiceID: "srv-1",
+						Inclusive: true,
+						Endpoints: map[string]IncludedEndpoint{
+							"edp-1": {
+								EndpointID: "edp-1",
+								Inclusive:  true,
+							},
+						},
+					},
+				},
+			},
+		},
+		BoundaryScope:    BoundaryScopeInbound,
+		Description:      "application-alert-config-description",
+		EvaluationType:   EvaluationTypePerApplication,
+		Granularity:      Granularity600000,
+		IncludeInternal:  false,
+		IncludeSynthetic: false,
+		CustomerPayloadFields: []CustomPayloadField{
+			{
+				Type:  StaticCustomPayloadType,
+				Key:   "static-key",
+				Value: "static-value",
+			},
+			{
+				Type:  DynamicCustomPayloadType,
+				Key:   "dynamic-key",
+				Value: "dynamic-value",
+			},
+		},
+		Name: "full-name",
+		Rule: ApplicationAlertRule{
+			AlertType:   "errorRate",
+			Aggregation: MinAggregation,
+			MetricName:  "metric-name",
+		},
+		Severity:            SeverityCritical.GetAPIRepresentation(),
+		TagFilterExpression: NewStringTagFilter(TagFilterEntitySource, "service.name", EqualsOperator, "test"),
+		Threshold: Threshold{
+			Type:        "staticThreshold",
+			Operator:    ThresholdOperatorGreaterThan,
+			LastUpdated: &thresholdLastUpdate,
+			Value:       &thresholdValue,
+		},
+		TimeThreshold: TimeThreshold{
+			Type:       "violationsInSequence",
+			TimeWindow: 1234,
+		},
+		Triggering: true,
+	}
+
+	serializedJSON, _ := json.Marshal(applicationAlertConfig)
+
+	result, err := NewApplicationAlertConfigUnmarshaller().Unmarshal(serializedJSON)
+
+	require.NoError(t, err)
+	require.Equal(t, &applicationAlertConfig, result)
+}
+
+func TestShouldFailToUnmarshalApplicationAlertConfigWhenResponseIsAJsonArray(t *testing.T) {
+	response := `["foo","bar"]`
+
+	_, err := NewApplicationAlertConfigUnmarshaller().Unmarshal([]byte(response))
+
+	require.Error(t, err)
+}
+
+func TestShouldReturnEmptyApplicationAlertConfigWhenNoFieldOfResponseMatchesToModel(t *testing.T) {
+	response := `{"foo" : "bar"}`
+	config, err := NewApplicationAlertConfigUnmarshaller().Unmarshal([]byte(response))
+
+	require.NoError(t, err)
+	require.Equal(t, &ApplicationAlertConfig{}, config)
+}
+
+func TestShouldFailToUnmarshalApplicationAlertConfigWhenResponseIsNotAValidJson(t *testing.T) {
+	response := `Invalid Data`
+
+	_, err := NewApplicationAlertConfigUnmarshaller().Unmarshal([]byte(response))
+
+	require.Error(t, err)
+}
