2f8f43c8da7ca64ee1efce085803ba6839f5c4f5
Florian Gessner <flo.gessner@gmail.com>
2022-04-18 20:09:04 +0200

    #126: fix lint error

--- instana/restapi/website-time-threshold.go
+++ instana/restapi/website-time-threshold.go
@@ -1,5 +1,6 @@
 package restapi
 
+//WebsiteTimeThreshold struct representing the API model of a website time threshold
 type WebsiteTimeThreshold struct {
 	Type                    string                          `json:"type"`
 	TimeWindow              *int64                          `json:"timeWindow"`
