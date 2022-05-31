aa9a5f21d519fab7ce96443fabd31621ca008c16
Florian Geßner <flo.gessner@gmail.com>
2022-05-31 09:47:01 +0200

    #106: add data structure and unmarshaller for CustomDashboard

---
+++ instana/restapi/custom-dashboard.go
@@ -0,0 +1,19 @@
+package restapi
+
+type CustomDashboard struct {
+	ID          string       `json:"id"`
+	Title       string       `json:"title"`
+	AccessRules []AccessRule `json:"accessRules"`
+	Widgets     string       `json:"widgets"`
+}
+
+//GetIDForResourcePath implementation of the interface InstanaDataObject for CustomDashboard
+func (a *CustomDashboard) GetIDForResourcePath() string {
+	return a.ID
+}
+
+//Validate implementation of the interface InstanaDataObject for CustomDashboard
+func (a *CustomDashboard) Validate() error {
+	//No validation required validation part of terraform schema
+	return nil
+}
